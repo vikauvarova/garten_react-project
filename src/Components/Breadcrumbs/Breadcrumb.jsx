2fbd54cbf58203e23bafa95b03dd2ba8e14bb27f
vikauvarova <114510911+vikauvarova@users.noreply.github.com>
2023-07-27 17:52:42 +0200

    update

--- src/Components/Breadcrumbs/Breadcrumb.jsx
+++ src/Components/Breadcrumbs/Breadcrumb.jsx
@@ -1,11 +1,72 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Link, useLocation, useParams } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 function Breadcrumb() {
 
     const location = useLocation();
     const pathnames = location.pathname.split("/").filter((path) => path);
+    const categoryName = useSelector(state => state.categories.category.title);
+    const productName = useSelector(state => state.products.product.title);
+
+  console.log(productName);
+
+    if(pathnames[0] === "categories" && pathnames.length > 1){
+      return (
+
+        <div className='breadcrumbs-container container'>
+            {pathnames.length > 0 && (
+              <>
+               {""}
+               <Link to="/">Home </Link> 
+               <span> / </span>{""}
+              </>
+            )}
+          {pathnames.map((pathname, index) => {
+              const linkTo = `${pathnames.slice(0, index + 1).join("/")}`;
+              const last = index === pathnames.length - 1;
+              return (
+                <div key={index}>
+                  {last ? (
+                      <span>{categoryName}</span>) : (<Link to={linkTo}>{pathname}</Link>
+                    )
+                  }
+                  {!last && <span> / </span>}
+                </div>
+              )
+            })
+          }
+        </div>
+      )
+    }
+    if(pathnames[0] === "products" && pathnames.length > 1){
+      return (
+
+        <div className='breadcrumbs-container container'>
+            {pathnames.length > 0 && (
+              <>
+               {""}
+               <Link to="/">Home </Link> 
+               <span> / </span>{""}
+              </>
+            )}
+          {pathnames.map((pathname, index) => {
+              const linkTo = `${pathnames.slice(0, index + 1).join("/")}`;
+              const last = index === pathnames.length - 1;
+              return (
+                <div key={index}>
+                  {last ? (
+                      <span>{productName}</span>) : (<Link to="all-products">{pathname}</Link>
+                    )
+                  }
+                  {!last && <span> / </span>}
+                </div>
+              )
+            })
+          }
+        </div>
+      )
+    }
 
   return (
 
@@ -20,7 +81,6 @@ function Breadcrumb() {
       {pathnames.map((pathname, index) => {
           const linkTo = `${pathnames.slice(0, index + 1).join("/")}`;
           const last = index === pathnames.length - 1;
-
           return (
             <div key={index}>
               {last ? (
